2ed29b774210f7222d0ef4f7f363292aa28b7dd7
MizanRbf <mizanrbf@gmail.com>
2025-08-31 20:35:33 +0600

    register page created and register info send to db

--- src/app/Components/Register/RegisterForm.jsx
+++ src/app/Components/Register/RegisterForm.jsx
@@ -1,8 +1,25 @@
+"use client";
+import { registerUser } from "@/app/actions/auth/registerUser";
 import React from "react";
 
 const RegisterForm = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    const result = await registerUser({ name, email, password });
+
+    if (result.success) {
+      alert("success");
+    } else {
+      alert("something wrong");
+    }
+  };
   return (
-    <form className="space-y-3">
+    <form onSubmit={handleSubmit} className="space-y-3">
       <p className="text-3xl font-bold bg-[#003b3d] text-white py-3 text-center rounded-sm">
         Register Your Account
       </p>
@@ -10,22 +27,28 @@ const RegisterForm = () => {
       {/* Name */}
       <input
         type="text"
+        name="name"
         className="bg-white p-3 rounded-sm w-full"
         placeholder="Your Name"
       />
       <input
         type="email"
+        name="email"
         className="bg-white p-3 rounded-sm w-full"
         placeholder="Your Email"
       />
 
       <input
         type="password"
+        name="password"
         className="bg-white p-3 rounded-sm w-full"
         placeholder="Password"
       />
 
-      <button className="text-white font-bold rounded-sm px-3 py-2 bg-[#003b3d] w-full">
+      <button
+        type="submit"
+        className="text-white font-bold rounded-sm px-3 py-2 bg-[#003b3d] w-full"
+      >
         Register
       </button>
     </form>
---
+++ src/app/actions/auth/registerUser.js
@@ -0,0 +1,29 @@
+"use server";
+
+import dbConnect from "@/lib/dbConnect";
+import bcrypt from "bcrypt";
+
+export const registerUser = async (payload) => {
+  // validation
+  const { email, password } = payload;
+  if (!email || !password) return { success: false };
+
+  const userCollection = dbConnect("users");
+
+  const user = await userCollection.findOne({ email: payload.email });
+
+  if (!user) {
+    const hashedPassword = bcrypt.hash(password, 10);
+    payload.password = hashedPassword;
+    const result = await userCollection.insertOne(payload);
+    const { acknowledged, insertedId } = result;
+
+    return {
+      success: true,
+      acknowledged,
+      insertedId: insertedId.toString(),
+    };
+  }
+
+  return { success: false, message: "User Already Exist" };
+};
