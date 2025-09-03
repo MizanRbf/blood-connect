65949aeafb34d1f7e84675ee3089c6f2996d3638
MizanRbf <mizanrbf@gmail.com>
2025-09-03 16:40:35 +0600

    filtered functon done

--- src/app/donors/page.js
+++ src/app/donors/page.js
@@ -6,11 +6,34 @@ import Navbar from "../Components/Navbar/Navbar";
 
 const donors = () => {
   const [donorsInfo, setDonorsInfo] = useState([]);
+  const [filteredDonors, setFilteredDonors] = useState([]);
+  const [selectedBlood, setSelectedBlood] = useState("");
 
+  // Handle Filter
+  const handleFilter = (e) => {
+    e.preventDefault();
+    const value = e.target.value;
+    setSelectedBlood(value);
+
+    // Filter
+    if (value === "") {
+      setFilteredDonors(donorsInfo);
+    } else {
+      const filtered = donorsInfo.filter(
+        (singleDonor) => singleDonor.blood_group === value
+      );
+      setFilteredDonors(filtered);
+    }
+  };
+
+  // All Donors Info
   useEffect(() => {
     fetch("/DonorsData.json")
       .then((res) => res.json())
-      .then((data) => setDonorsInfo(data));
+      .then((data) => {
+        setDonorsInfo(data);
+        setFilteredDonors(data);
+      });
   }, []);
   return (
     <div className="max-w-[1500px] mx-auto px-4 pt-20 mb-20">
@@ -22,8 +45,22 @@ const donors = () => {
 
       {/* Filter and Sort */}
       <div className="flex justify-between mb-10 items-center">
+        {/* Searchbar */}
+        <form className="">
+          <input
+            type="search"
+            name="Search"
+            placeholder="Search By Location"
+            className="border w-50 rounded-sm p-2"
+          />
+        </form>
+
         {/* Filter */}
-        <select className="border w-50 rounded-sm p-2" defaultValue="">
+        <select
+          className="border w-50 rounded-sm p-2"
+          value={selectedBlood}
+          onChange={handleFilter}
+        >
           <option value="" disabled>
             Filter By Blood Group
           </option>
@@ -36,24 +73,11 @@ const donors = () => {
           <option value="O+">O+</option>
           <option value="O-">O-</option>
         </select>
-
-        {/* Sort */}
-        <select className="border w-50 rounded-sm p-2" defaultValue="">
-          <option value="" disabled>
-            Sort By
-          </option>
-          <option value="newst">newst</option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-        </select>
       </div>
 
       {/* Cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {donorsInfo.map((donor) => (
+        {filteredDonors.map((donor) => (
           <DonorCard key={donor.id} donor={donor}></DonorCard>
         ))}
       </div>
