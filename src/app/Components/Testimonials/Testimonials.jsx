df38d6c4a6a82262c6fe38c6c518a2a94fbe14a4
MizanRbf <mizanrbf@gmail.com>
2025-09-18 23:41:58 +0600

    testimonials design updated

--- src/app/Components/Testimonials/Testimonials.jsx
+++ src/app/Components/Testimonials/Testimonials.jsx
@@ -26,28 +26,27 @@ const Testimonials = () => {
     },
   ];
   return (
-    <div></div>
-    <div className="max-w-[1500px] mx-auto px-4 pt-20 pb-10 bg-[url(/assets/bg1.jpg)] bg-center bg-cover">
-      <Title title="Testimonials"></Title>
-      <section className=" p-6 md:p-12 bg-white rounded-2xl shadow-md mt-12">
-        <div className="grid gap-6 grid-cols-1 md:grid-cols-3">
-          {testimonials.map((t) => (
-            <div
-              key={t.id}
-              className="flex flex-col bg-red-50 rounded-xl p-6 ring-1 ring-red-100 hover:shadow-lg transition-shadow"
-            >
-              <Quote className="w-8 h-8 text-red-500 mb-3" />
-              <p className="text-sm text-gray-700 italic mb-4">
-                “{t.feedback}”
-              </p>
-              <div className="mt-auto">
-                <p className="font-medium text-red-700">{t.name}</p>
-                <p className="text-xs text-gray-600">{t.role}</p>
+    <div className="bg-[url(/assets/bg1.jpg)] bg-center bg-cover">
+      <div className="max-w-[1500px] mx-auto px-4 pt-10 pb-10 ">
+        <Title title="Testimonials"></Title>
+        <section className=" p-6 md:p-12 bg-[#00000041] rounded-2xl shadow-md mt-12">
+          <div className="grid gap-6 grid-cols-1 md:grid-cols-3">
+            {testimonials.map((t) => (
+              <div
+                key={t.id}
+                className="flex flex-col bg-[#ff505075] rounded-xl p-6 hover:shadow-lg transition-shadow"
+              >
+                <Quote className="w-8 h-8 text-white mb-3" />
+                <p className="text-sm text-white italic mb-4">“{t.feedback}”</p>
+                <div className="mt-auto">
+                  <p className="font-medium text-white">{t.name}</p>
+                  <p className="text-xs text-white">{t.role}</p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </section>
+            ))}
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
